e7fce13f804d0c05c582fda8b016c8ecbeda0d9a
amrahov <164860131+amrahov@users.noreply.github.com>
2024-04-07 23:08:25 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,9 +1,18 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  month=input("Enter name of the month [ex. June]: ")
+  day=int(input("Enter the day [ex. 5]: "))
+  if (month=="March" and day>=20) or month=="April" or month=="May" or (month=="June" and day<21):
+      print(f"{month} {day} is in Spring")
+  elif (month=="June" and day>=21) or month=="July" or month=="August" or (month=="September" and day<22):
+      print(f"{month} {day} is in Summer")
+  elif (month=="September" and day>=22) or month=="October" or month=="November" or (month=="December" and day<21):
+      print(f"{month} {day} is in Fall")
+  elif (month=="December" and day>=21) or month=="January" or month=="Febrary" or(month=="March" and day<20):
+      print(f"{month} {day} is in Winter")
+  else:
+      print("Invalid input or date.")
   pass
 
 if __name__ == "__main__":
